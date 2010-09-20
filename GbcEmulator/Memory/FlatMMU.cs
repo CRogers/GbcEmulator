1fddb0e40f02dec27eeb533bb3dac719907c21b1
Callum Rogers <callumrogers@hotmail.co.uk>
2010-09-20 22:14:11 +0100

    Added a new "Flat" MMU for easy debugging and testing of everything except the main memory

---
+++ GbcEmulator/Memory/FlatMMU.cs
@@ -0,0 +1,33 @@
+﻿namespace GbcEmulator.Memory
+{
+    public class FlatMMU : IMemoryManagementUnit
+    {
+        private byte[] ram;
+
+        public FlatMMU(int size)
+        {
+            ram = new byte[size];
+        }
+
+        public byte ReadByte(int addr)
+        {
+            return ram[addr];
+        }
+
+        public ushort ReadUShort(int addr)
+        {
+            return (ushort)(ram[addr] | (ram[addr+1] << 8));
+        }
+
+        public void WriteByte(int addr, byte value)
+        {
+            ram[addr] = value;
+        }
+
+        public void WriteUShort(int addr, ushort value)
+        {
+            ram[addr] = (byte)(value & 0xFF);
+            ram[addr + 1] = (byte)(value >> 8);
+        }
+    }
+}
